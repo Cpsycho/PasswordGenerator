ea2e63cea1b764e71a25974d1197f8931871d61a
cyber psycho <165747806+Cpsycho@users.noreply.github.com>
2024-05-23 10:13:03 +0600

    Add files via upload

---
+++ PG.py
@@ -0,0 +1,336 @@
+import time
+
+t = time.localtime(time.time())
+formatted_time = time.strftime("\n\t\t\t\tTIME: %I:%M %p", t)
+print("\33[33m")
+print(formatted_time); print("\33[0m")
+banner = """
+
+
+\x1b[36m 
+ █──░█ █▀▀█ █▀▀█ █▀▀▄ █── ─▀─ █▀▀ ▀▀█▀▀ 
+░█░█░█ █──█ █▄▄▀ █──█ █── ▀█▀ ▀▀█ ──█── 
+ █▄▀▄█ ▀▀▀▀ ▀─▀▀ ▀▀▀─ ▀▀▀ ▀▀▀ ▀▀▀ ──▀──
+         
+  █▀▄▀█ ▄▀█ █▄▀ █▀▀ █▀█
+  █░▀░█ █▀█ █░█ ██▄ █▀▄
+  
+  \x1b[37mᶜʳᵉᵃᵗᵉᵈ ᵇʸ ᶜʸᵇᵉʳ~ᵖˢʸᶜʰᵒ
+"""
+
+def animate_banner(banner):
+    lines = banner.split('\n')
+    for line in lines:
+        for char in line:
+            print(char, end='', flush=True)
+            time.sleep(0.004)  # Adjust the sleep time to change the animation speed
+        print()  # Move to the next line after printing a line
+
+# Animate the banner
+animate_banner(banner);
+
+time.sleep(1)
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Call the function to clear the screen
+
+time.sleep(0.5)
+
+print("\n")
+print("\x1b[32m")
+victim_stname=input("\n\x1b[40mEnter victim's first name:")
+time. sleep(0.11)
+victim_lastname=input("Last name:")
+time. sleep(0.22)
+mobile=input("\nMobile number:")
+time. sleep(0.22)
+birthday=input(" \nBirthday:")
+time. sleep(0.22)
+birthyear=input("\nBirth year:")
+time. sleep(0.22)
+fathername=input("\nEnter father's first name:")
+time. sleep(0.11)
+fatherlastname=input("Last name:")
+time. sleep(0.22)
+mothername=input("\nEnter Mother's first name:");
+time. sleep(0.11)
+motherlastname=input("Last name:")
+time. sleep(0.22)
+f=input("\nEnter favourite person's first name:")
+time. sleep(0.11)
+favlastname=input("Last name:")
+print("\x1b[0m")
+time. sleep(0.3)
+clear_screen()
+#number combination.... 
+n1="123";
+n2="321";
+n3="1234";
+n4="4321";
+n5="12345";
+n6="54321";
+n7="123456";
+n8="654321";
+n9="123456789";
+n10="987654321";
+n11="789";
+n12="987";
+n13="6789";
+n14="9876";
+
+import sys
+import time
+
+def loading_animation():
+    for _ in range(5):  # Adjust the range based on the desired duration of the loading animation
+        sys.stdout.write("\n\n\r\x1b[33mPassword generating " + "...." * _)
+        
+
+        sys.stdout.flush()
+        time.sleep(0.5)  # Adjust the sleep time to control the speed of the animation
+
+
+# Call the function before compiling the results
+loading_animation()
+#name to numeric code;
+print("\x1b[33m");
+time. sleep(0.3);
+print("\n\n\n")
+
+#name numeric value
+
+def string_to_numeric_code(s):
+    numeric_code = ""
+    for char in s:
+        if char.isalpha():
+            numeric_value = ord(char.lower()) - ord('a') + 1
+            numeric_code += str(numeric_value)
+    return numeric_code
+victim_stnamen=victim_stname;
+numeric_code = string_to_numeric_code(victim_stnamen)
+print(numeric_code)
+print(victim_stname+numeric_code)
+print(victim_lastname+numeric_code)
+
+def string_to_numeric_code(s):
+    numeric_code = ""
+    for char in s:
+        if char.isalpha():
+            numeric_value = ord(char.lower()) - ord('a') + 1
+            numeric_code += str(numeric_value)
+    return numeric_code
+print("\n")
+string =victim_lastname
+numeric_code = string_to_numeric_code(string)
+print(numeric_code);print("\x1b[36m")
+print(victim_stname+numeric_code);print(victim_lastname+numeric_code)
+print("\n\n\x1b[0m")
+
+
+print("\x1b[35m");
+
+
+
+print(victim_stname+n1);
+print(victim_lastname+n1);
+print( victim_stname+n2);
+print(victim_lastname+n2);
+print(victim_stname+n3)
+print(victim_lastname+n3)
+print(victim_stname+n4)
+print(victim_lastname+n4)
+print(victim_stname+n5)
+print(victim_lastname+n5)
+print(victim_stname+n6)
+time. sleep(0.2);
+print(victim_lastname+n6)
+print(victim_stname+n7)
+print(victim_lastname+n7)
+print(victim_stname+n8)
+print(victim_lastname+n8)
+print(victim_stname+n9)
+print("\x1b[0m")
+print("\x1b[32m")
+time. sleep(0.1);
+print(victim_lastname+n9)
+print(victim_stname+n10)
+print(victim_lastname+n10)
+print(victim_stname+n11)
+print(victim_lastname+n11)
+print(victim_stname+n12)
+print(victim_lastname+n12);print(victim_stname+n13)
+print(victim_lastname+n13);print(victim_stname+n14)
+print(victim_lastname+n14);
+print(victim_stname+birthday)
+print(victim_lastname+birthday)
+print(victim_stname+birthyear);print(victim_lastname+birthyear)
+print("\n")
+print(victim_stname+mobile[7:11])
+print(victim_lastname+mobile[7:11])
+print(victim_stname+mobile[8:11])
+print(victim_stname+mobile[8:11])
+print(victim_stname+mobile);print(victim_lastname+mobile)
+
+time. sleep(0.9989899)
+print("\n")
+
+#upper case
+
+upperst = victim_stname.upper()
+print(upperst+n1)
+print(upperst+n2); print(upperst+n3)
+print(upperst+n4);print(upperst+n5)
+print(upperst+n6); print(upperst+n7)
+print(upperst+n8);print(upperst+n9)
+print(upperst+n10) ;print(upperst+n11);print(upperst+n12)
+print(upperst+n13);print(upperst+n14) 
+print(upperst+birthday);print(upperst+birthyear)
+print("\n")
+print(upperst+mobile [7:11]);print(upperst+mobile[8:11])
+print(upperst+mobile)
+time. sleep(0.9998997)
+print("\n")
+#victim_lastname upper
+
+upperlast = victim_lastname.upper()
+print(upperlast+n1);print(upperlast+n2)
+print(upperlast+n3);print(upperlast+n4)
+print(upperlast+n5);print(upperlast+n6)
+print(upperlast+n7);print(upperlast+n8)
+print(upperlast+n9);print(upperlast+n10)
+print(upperlast+n11);print(upperlast+n12)
+print(upperlast+n13);print(upperlast+n14)
+print(upperlast+birthday);print(upperlast+birthyear)
+print("\n")
+print(upperlast+mobile[7:11])
+print(upperlast+mobile[8:11]);print(upperlast+mobile)
+time. sleep(0.999989889)
+print("\n")
+#fathername
+print(fathername+n1);print(fatherlastname+n1)
+print(fathername+n2);print(fatherlastname+n2)
+print(fathername+n3);print(fatherlastname+n3)
+print(fathername+n4);print(fatherlastname+n4)
+print(fathername+n5);print(fatherlastname+n5)
+print(fathername+n6);print(fatherlastname+n6)
+print(fathername+n7);print(fatherlastname+n7)
+print(fathername+n8);print(fatherlastname+n8)
+print(fathername+n9);print(fatherlastname+n9)
+print(fathername+n10);print(fatherlastname+n10)
+print(fathername+n11);print(fatherlastname+n11)
+print(fathername+n12);print(fathername+n12)
+print(fathername+n13);print(fatherlastname+n13)
+print(fathername+n14);print(fatherlastname+n14)
+print(fathername+mobile[8:11]);print(fatherlastname+mobile[8:11])
+print(fathername+mobile);print(fatherlastname+mobile)
+print("\n\x1b[37m")
+time. sleep(0.9898889)
+#fathername upper
+lofa=fatherlastname.upper()
+upfa=fathername.upper()
+print(upfa+n1);print(lofa+n1)
+print(upfa+n2);print(lofa+n2)
+print(upfa+n3);print(lofa+n3)
+print(upfa+n4);print(lofa+n4)
+print(upfa+n5);print(lofa+n5)
+print(upfa+n6);print(lofa+n6)
+print(upfa+n7);print(lofa+n7)
+print(upfa+n8);print(lofa+n8)
+print(upfa+n9);print(lofa+n9)
+print(upfa+n10);print(lofa+n10)
+print(upfa+n11);print(lofa+n11)
+print(upfa+n12);print(lofa+n12)
+print(upfa+n13);print(lofa+n13)
+print(upfa+n14);print(lofa+n14)
+print(upfa+mobile[8:11]);print(upfa+mobile)
+print("\n\x1b[0m" )
+#Mothers name
+print(mothername+n1);print(motherlastname+n1)
+print(mothername+n2);print(motherlastname+n2)
+print(mothername+n3);print(motherlastname+n3)
+print(mothername+n4);print(motherlastname+n4)
+print(mothername+n5);print(motherlastname+n5)
+print(mothername+n6);print(motherlastname+n6)
+print(mothername+n7);print(motherlastname+n7)
+print(mothername+mobile[8:11]);print(motherlastname+mobile[8:11])
+print(mothername+mobile);print(motherlastname+mobile)
+print("\n\x1b[33m")
+time. sleep(0.989889)
+print(mothername+n8);print(motherlastname+n8)
+print(mothername+n9);print(motherlastname+n9)
+print(mothername+n10);print(motherlastname+n10)
+print(mothername+n11);print(motherlastname+n11)
+print(mothername+n12);print(motherlastname+n12)
+print(mothername+n13);print(motherlastname+n13)
+print(mothername+n14);print(motherlastname+n14)
+print(mothername+mobile[8:11]);print(motherlastname+mobile[8:11])
+print(mothername+mobile);print(motherlastname+mobile)
+#motherlastname upper
+upma=motherlastname. upper()
+print("\n")
+print(upma+n1);print(upma+n2)
+print(upma+n3);print(upma+n3)
+print(upma+n4);print(upma+n5)
+print(upma+n6);print(upma+n7)
+print(upma+n8);print(upma+n9)
+print("\x1b[0m") 
+print("\x1b[32m")
+print(upma+n10);print(upma+n11)
+print(upma+n12);print(upma+n13)
+print(upma+n14);
+print(upma+mobile[8:11]);print(upma+mobile)
+#favourite person name
+time. sleep(0.1222349)
+print("\n")
+
+print(f+n1);print(favlastname+n1)
+print(f+n2);print(favlastname+n2)
+print(f+n3);print(favlastname+n3)
+print(f+n4);print(favlastname+n4)
+print(f+n5);print(favlastname+n5)
+print(f+n6);print(favlastname+n6)
+print(f+n7);print(favlastname+n7)
+print(f+n8);print(favlastname+n8)
+print(f+n9);print(favlastname+n9)
+print(f+n10);print(favlastname+n10)
+print(f+n11);print(favlastname+n11)
+print(f+n12);print(favlastname+n12)
+print(f+n13);print(favlastname+n1)
+print("\n\x1b[34m")
+#favourite person name upper
+upfv=f. upper()
+lofv=favlastname. upper()
+time. sleep(0.9898798)
+print(upfv+n1);print(lofv+n1);
+print(upfv+n2);print(lofv+n2)
+print (upfv+n3);print(lofv+n3)
+print(upfv+n4);print(lofv+n4);
+print(upfv+n5);print(lofv+n5)
+print (upfv+n6);print(lofv+n6);
+print(upfv+n7);print(lofv+n7);
+print(upfv+n8);print(lofv+n8)
+print (upfv+n9);print(lofv+n9);
+print(upfv+n10);print(lofv+n10)
+print(upfv+n11);print(lofv+n12);
+print (upfv+n13);print(lofv+n13)
+print(upfv+n14);print(lofv+n14)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
